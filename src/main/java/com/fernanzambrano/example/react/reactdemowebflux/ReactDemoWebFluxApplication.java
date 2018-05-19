59aefd008b372e4e8f2d24d382a7cbe0ceb622df
Fernando <fernanzambrano@gmail.com>
2018-05-19 12:18:40 +0200

    First import files

---
+++ src/main/java/com/fernanzambrano/example/react/reactdemowebflux/ReactDemoWebFluxApplication.java
@@ -0,0 +1,12 @@
+package com.fernanzambrano.example.react.reactdemowebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactDemoWebFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactDemoWebFluxApplication.class, args);
+	}
+}
---
+++ src/test/java/com/fernanzambrano/example/react/reactdemowebflux/ReactDemoWebFluxApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fernanzambrano.example.react.reactdemowebflux;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReactDemoWebFluxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
